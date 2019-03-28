417bf5d81c6038de41e2cf04ddebb2c150d27284
Kazi Jawad <kazi_jawad@outlook.com>
2019-03-27 21:37:42 -0400

    Setup initial components

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,7 @@
-import Link from 'next/link';
-
 function Home() {
 	return (
 		<div>
 			<p>Hey!</p>
-			<Link href="/sell">
-				<a>Sell!</a>
-			</Link>
 		</div>
 	);
 }
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,12 +1,7 @@
-import Link from 'next/link';
-
 function Sell() {
 	return (
 		<div>
 			<p>Sell!</p>
-			<Link href="/">
-				<a>Home!</a>
-			</Link>
 		</div>
 	);
 }
