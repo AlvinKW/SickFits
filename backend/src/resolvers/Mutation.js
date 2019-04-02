f0b732ab7e58b93f4540db259f144a11026d4246
Kazi Jawad <kazi_jawad@outlook.com>
2019-04-01 23:06:31 -0400

    Add queries and mutations to update an item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -5,6 +5,15 @@ const Mutation = {
 		const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
 		return item;
 	},
+	updateItem(parent, args, ctx, info) {
+		const updates = { ...args };
+		delete updates.id;
+
+		return ctx.db.mutation.updateItem({
+			data: updates,
+			where: { id: args.id },
+		}, info);
+	},
 };
 
 module.exports = Mutation;
