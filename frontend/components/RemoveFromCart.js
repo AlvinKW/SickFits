497d9efa16dc742d1eabfcfe945d1fcf9ba0f1f0
Kazi Jawad <kazi_jawad@outlook.com>
2019-06-06 18:19:02 -0400

    Optimistic Response & Cache Updates

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -26,11 +26,27 @@ const StyledButton = styled.button`
 `;
 
 class RemoveFromCart extends Component {
+	update = (cache, payload) => {
+		const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+		const cartItemID = payload.data.removeFromCart.id;
+
+		data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemID);
+		cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+	};
+
 	render() {
 		return (
 			<Mutation
 				mutation={REMOVE_FROM_CART_MUTATION}
 				variables={{ id: this.props.id }}
+				update={this.update}
+				optimisticResponse={{
+					__typename: 'Mutation',
+					removeFromCart: {
+						__typename: 'CartItem',
+						id: this.props.id,
+					},
+				}}
 			>
 				{(removeFromCart, { loading }) => (
 					<StyledButton
