73327c83cff3bb30a5683ac6ccc4f1b7a64af9cd
Kazi Jawad <kazi_jawad@outlook.com>
2019-06-23 00:24:38 -0400

    Formatting

--- frontend/pages/auth.js
+++ frontend/pages/auth.js
@@ -4,7 +4,7 @@ import SignUp from '../components/SignUp';
 import SignIn from '../components/SignIn';
 import RequestReset from '../components/RequestReset';
 
-const Columns = styled.div`
+const StyledColumns = styled.div`
 	display: grid;
 	grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 	grid-gap: 20px;
@@ -12,11 +12,11 @@ const Columns = styled.div`
 
 function Auth() {
 	return (
-		<Columns>
+		<StyledColumns>
 			<SignUp />
 			<SignIn />
 			<RequestReset />
-		</Columns>
+		</StyledColumns>
 	);
 }
 
