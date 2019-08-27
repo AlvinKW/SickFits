81632aa9964327d573a8a2824e28fe689c30d437
Kazi Jawad <kazi_jawad@outlook.com>
2019-08-26 23:33:35 -0400

    Fix deployment

---
+++ backend/src/writeSchema.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const { importSchema } = require('graphql-import');
+
+const text = importSchema('src/generated/prisma.graphql');
+fs.writeFileSync('src/schema_prep.graphql', text);
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	env: {
-		ENDPOINT: 'http://localhost:5000',
+		ENDPOINT: 'https://sickfits-backend.kazijawad.now.sh',
 		PERPAGE: 4,
 		STRIPE_KEY: 'pk_test_m7FuPcR7nXOWy7CI8nDQUhsw00df1XeHRB',
 	},
